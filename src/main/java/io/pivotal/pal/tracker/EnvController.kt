6fb409bf20b1fcb775091c4a5be5a62f210765db
sbecket5 <sbecket5@ford.com>
2021-03-02 11:25:09 -0500

    Local Update

--- src/main/java/io/pivotal/pal/tracker/EnvController.kt
+++ src/main/java/io/pivotal/pal/tracker/EnvController.kt
@@ -1,11 +1,27 @@
 package io.pivotal.pal.tracker
 
-class EnvController(private val str1: String, private val str2: String, private val str3: String, private val str4: String ) {
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-    val env: Map<String, String> = mapOf(
-        "PORT" to str1,
-        "MEMORY_LIMIT" to str2,
-        "CF_INSTANCE_INDEX" to str3,
-        "CF_INSTANCE_ADDR" to str4
-    )
+@RestController
+class EnvController(
+    @Value("\${port:NOT SET}")
+    private val port: String,
+    @Value("\${memory.limit:NOT SET}")
+    private val memoryLimit: String,
+    @Value("\${cf.instance.index:NOT SET}")
+    private val cfIndex: String,
+    @Value("\${cf.instance.addr:NOT SET}")
+    private val cfAddress: String ) {
+
+    @GetMapping("/env")
+    fun getEnv(): Map<String, String> {
+        return mapOf(
+            "PORT" to port,
+            "MEMORY_LIMIT" to memoryLimit,
+            "CF_INSTANCE_INDEX" to cfIndex,
+            "CF_INSTANCE_ADDR" to cfAddress
+        )
+    }
 }
