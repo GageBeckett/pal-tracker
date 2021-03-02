db01fbc605de911630acc00796a7e6230396aa48
sbecket5 <sbecket5@ford.com>
2021-03-02 10:39:57 -0500

    added welcome parameter

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.kt
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.kt
@@ -1,11 +1,14 @@
 package io.pivotal.pal.tracker
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class WelcomeController {
 
+    @Value("#{welcome.message}")
+    lateinit var welcomeMessage: String
 
     @GetMapping
     fun sayHello(): String {
