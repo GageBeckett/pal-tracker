d63f5e534b0365ba5c112b6f569d2d2f580280da
sbecket5 <sbecket5@ford.com>
2021-03-03 10:48:02 -0500

    Local Update

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
@@ -2,9 +2,15 @@ package io.pivotal.pal.tracker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class PalTrackerApplication
     fun main(args: Array<String>) {
         runApplication<PalTrackerApplication>(*args)
+    }
+
+    @Bean
+    fun timeEntryRepository(): TimeEntryRepository {
+        return InMemoryTimeEntryRepository()
     }
--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.kt
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.kt
@@ -2,32 +2,41 @@ package io.pivotal.pal.tracker
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
+@RestController
 class TimeEntryController(private val timeEntryRepository: TimeEntryRepository) {
 
-    fun create(timeEntry: TimeEntry?):ResponseEntity<TimeEntry> {
-        //TODO("Not yet implemented")
-        return ResponseEntity(HttpStatus.ACCEPTED)
+    @PostMapping("/create")
+    @ResponseBody
+    fun create(@RequestBody timeEntry: TimeEntry?):ResponseEntity<TimeEntry> =
+        timeEntryRepository.create(timeEntry).let {
+            ResponseEntity<TimeEntry>(it, HttpStatus.CREATED)
+        }
+
+    @GetMapping("/read")
+    @ResponseBody
+    fun read(@RequestParam("id") id: Long):ResponseEntity<TimeEntry> =
+        timeEntryRepository.find(id)?.let {
+            ResponseEntity.ok(it)
+        } ?: ResponseEntity(HttpStatus.NOT_FOUND)
+
+    @GetMapping("/list")
+    fun list():ResponseEntity<MutableList<TimeEntry>> = timeEntryRepository.list().let {
+        ResponseEntity.ok(it)
     }
 
-    fun read(id: Long):ResponseEntity<TimeEntry> {
-        //TODO("Not yet implemented")
-        return ResponseEntity(HttpStatus.ACCEPTED)
-    }
+    @PutMapping("/update")
+    @ResponseBody
+    fun update(@RequestParam("id") id: Long,
+               @RequestBody timeEntry: TimeEntry):ResponseEntity<TimeEntry> =
+        timeEntryRepository.update(id, timeEntry)?.let {
+            ResponseEntity.ok(it)
+        } ?: ResponseEntity.notFound().build()
 
-    fun list():ResponseEntity<MutableList<TimeEntry>> {
-        //TODO("Not yet implemented")
-        return ResponseEntity(HttpStatus.ACCEPTED)
-    }
-
-    fun update(id: Long, timeEntry: TimeEntry?):ResponseEntity<TimeEntry> {
-        //TODO("Not yet implemented")
-        return ResponseEntity(HttpStatus.ACCEPTED)
-    }
-
-    fun delete(id: Long): ResponseEntity<Void> {
-        //TODO("Not yet implemented")
-        return ResponseEntity(HttpStatus.ACCEPTED)
-    }
+    @DeleteMapping("/delete")
+    fun delete(@RequestParam("id") id: Long): ResponseEntity<Void> =
+        ResponseEntity.noContent().build<Void>().also {
+            timeEntryRepository.delete(id)
+        }
 }
