8d9c4045f8fd3f26a5f25bef8a631bdffd985441
sbecket5 <sbecket5@ford.com>
2021-03-03 11:17:06 -0500

    local update

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
@@ -2,17 +2,10 @@ package io.pivotal.pal.tracker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.support.beans
 
 @SpringBootApplication
 class PalTrackerApplication
 
     fun main(args: Array<String>) {
         runApplication<PalTrackerApplication>(*args)
-        beans {
-            bean<TimeEntryRepository> {
-                InMemoryTimeEntryRepository()
-            }
-        }
     }
