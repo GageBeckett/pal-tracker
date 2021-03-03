099310aaea777ddcf7cb9e1a28c53f3c1480f3e9
sbecket5 <sbecket5@ford.com>
2021-03-03 15:27:01 -0500

    convert TimeEntry to dataclass

--- src/main/java/io/pivotal/pal/tracker/TimeEntry.kt
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.kt
@@ -3,26 +3,4 @@ package io.pivotal.pal.tracker
 import java.time.LocalDate
 import java.util.*
 
-class TimeEntry(val id: Long, val projectId: Long, val userId: Long, val date: LocalDate, val hours: Int) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || javaClass != other.javaClass) return false
-        val timeEntry = other as TimeEntry
-        return id == timeEntry.id && projectId == timeEntry.projectId && userId == timeEntry.userId && hours == timeEntry.hours && date == timeEntry.date
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(id, projectId, userId, date, hours)
-    }
-
-    override fun toString(): String {
-        return "TimeEntry{" +
-                "id=" + id +
-                ", projectId=" + projectId +
-                ", userId=" + userId +
-                ", date=" + date +
-                ", hours=" + hours +
-                '}'
-    }
-}
+data class TimeEntry(val id: Long, val projectId: Long, val userId: Long, val date: LocalDate, val hours: Int)
