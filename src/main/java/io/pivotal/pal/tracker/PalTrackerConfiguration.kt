bd5e7b03b1fdbe54c36dc0035c85cd8b0a369fbb
sbecket5 <sbecket5@ford.com>
2021-03-04 12:31:12 -0500

    done

--- src/main/java/io/pivotal/pal/tracker/PalTrackerConfiguration.kt
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerConfiguration.kt
@@ -2,10 +2,11 @@ package io.pivotal.pal.tracker
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
 
 @Configuration
 class PalTrackerConfiguration {
 
     @Bean
-    fun timeEntryRepository(): TimeEntryRepository = InMemoryTimeEntryRepository()
+    fun timeEntryRepository(dataSource: DataSource): TimeEntryRepository = JdbcTimeEntryRepository(dataSource)
 }
